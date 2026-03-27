499f4df84bdad13e5f62b09d70788150d6f16344
wreck046 <ivankoroh46@gmail.com>
2026-03-27 10:28:28 +0800

    adding api service on render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://siakad-api.onrender.com",
+  //baseURL: "http://localhost:8080/api",
 });
 
 // REQUEST → inject token
