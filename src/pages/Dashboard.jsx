a3ac850d858757916d1d138823c3b14c0fdc367f
wreck046 <ivankoroh46@gmail.com>
2026-03-29 20:34:02 +0800

    adjusting the style

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -47,17 +47,17 @@ export default function Dashboard() {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         <div className="bg-white p-5 rounded-2xl shadow hover:shadow-lg transition">
-          <p className="text-gray-500 mb-2">Total Siswa</p>
-          <h2 className="text-3xl text-gray-900 font-bold">{totalSiswa}</h2>
+          <p className="text-gray-200 mb-2">Total Siswa</p>
+          <h2 className="text-3xl text-gray-500 font-bold">{totalSiswa}</h2>
         </div>
 
         <div className="bg-white p-5 rounded-2xl shadow hover:shadow-lg transition">
-          <h2 className="text-black mb-2">Data Aktif</h2>
+          <h2 className="text-gray-200 mb-2">Data Aktif</h2>
           <p className="text-3xl font-bold text-green-500">Aktif</p>
         </div>
 
         <div className="bg-white p-5 rounded-2xl shadow hover:shadow-lg transition">
-          <h2 className="text-black mb-2">Status</h2>
+          <h2 className="text-gray-200 mb-2">Status</h2>
           <p className="text-3xl font-bold text-blue-500">Online</p>
         </div>
       </div>
